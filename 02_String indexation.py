429a28c5c9be7ead8de96e03a39ea938c15c954b
xyanpablo <xyanpablo671@gfmail.com>
2024-08-11 15:45:16 +0300

    11.08

---
+++ 02_String indexation.py
@@ -0,0 +1,8 @@
+name = 'Denis'
+print('Hello, ' + name)     # strings and variables can be concatenated
+print((name + ', ') * 5)    # ... and multiplied
+print(name[0])
+print(name[-1])
+print(name[0:3])
+print(name[0:5:2])          # 0 - home; 5 - end; 2 - step
+print(name[::-1])
