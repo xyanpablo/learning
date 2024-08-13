d4d6ef91c35e493f9e4a62f76d7160d89095f55d
xyanpablo <xyanpablo671@gfmail.com>
2024-08-13 22:04:16 +0300

    12.08

--- 02_variables_task.py
+++ 02_variables_task.py
@@ -1,6 +1,6 @@
-hw_amount = '12'
-hours_spent = '1.5'
+hw_amount = 12
+hours_spent = 1.5
 course = 'Python'
-tasktime = float(hours_spent) / int(hw_amount)
+tasktime = hours_spent / hw_amount
 print('Курс:', course, ', всего задач: ', hw_amount, ', затрачено часов:', hours_spent, ', среднее время выполнения:',
       tasktime, 'часа.')
