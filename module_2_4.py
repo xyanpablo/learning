05114022a393a2b352233509c49c1fe549130f87
xyanpablo <xyanpablo671@gfmail.com>
2024-08-24 16:14:17 +0300

    12.08

---
+++ module_2_4.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = True
+
+for i in numbers:
+    for j in range(2, i):
+        if i % j == 0:
+            not_primes.append(i)
+            break
+        else:
+            primes.append(i)
+            break
+
+
+
+print( 'Primes: ', primes)
+print( 'Not primes: ', not_primes)
