9f6746ab9db7bf079876c74efcac7ab5d0e953de
xyanpablo <xyanpablo671@gfmail.com>
2024-08-25 00:04:05 +0300

    12.08

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        a = []
+        for j in range(m):
+            a.append(value)
+        matrix.append(a)
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
