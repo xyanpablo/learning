ed0280e6571f5e5dbfcbef24d3ee7f5163d741a7
xyanpablo <xyanpablo671@gfmail.com>
2024-08-27 00:36:52 +0300

    12.08

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.upper()
+    for i in list_to_search:
+        i = i.upper()
+        if string in i:
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
